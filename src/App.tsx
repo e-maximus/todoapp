33d2d714579aed2147f2646cdcd1cd33f662124c
Artem Kalashnikov <kalashnikov.tema@gmail.com>
2020-02-26 15:22:27 +0300

    Added todo list, add, toggle status. Basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import nanoid from 'nanoid'
+import * as todoReducer from './stores/todo'
+
+import styles from './App.module.scss';
 
 function App() {
+
+  const [state, dispatch] = useReducer(todoReducer.reducer, todoReducer.initialState())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.appContainer}>
+      {
+        state.todoList.map((todo) => {
+          return (
+            <div className={styles.todo + ' ' + todo.status} key={todo.id} onClick={() => {
+              dispatch({type: todoReducer.ACTION_TOGGLE_STATUS, todoId: todo.id})
+            }}>
+              {todo.title}
+            </div>
+          )
+        })
+      }
+      <button onClick={() => {
+        dispatch({type: todoReducer.ACTION_ADD, todo: {title: 'Title ' + Date.now(), createdAt: new Date(), id: nanoid(), status: todoReducer.STATUS_ACTIVE}})
+      }} >Add new todo</button>
     </div>
   );
 }
---
+++ src/stores/todo.tsx
@@ -0,0 +1,64 @@
+import nanoid from "nanoid";
+
+export const STATUS_ACTIVE = 'active'
+export const STATUS_DONE = 'done'
+
+export const ACTION_ADD = 'addTodo'
+export const ACTION_TOGGLE_STATUS = 'toggleStatus'
+
+export interface todo {
+  title: string,
+  createdAt: Date,
+  id: string,
+  status: 'active' | 'done'
+}
+
+export interface stateInterface {
+  todoList: Array<todo>
+}
+
+export const initialState = (): stateInterface => ({
+  todoList: [
+    {title: 'Install modules', createdAt: new Date(), id: nanoid(), status: STATUS_ACTIVE},
+    {title: 'Run this app', createdAt: new Date(), id: nanoid(), status: STATUS_ACTIVE},
+  ]
+})
+
+const sortTodo = (sortField = 'title') => (a: any | todo, b: any | todo): number => {
+  switch (true) {
+    case a[sortField] > b[sortField]:
+      return 1
+    case a[sortField] < b[sortField]:
+      return -1
+    default:
+      return 0
+  }
+}
+
+const sortedArray = (array: Array<todo>) => {
+  return array.sort(sortTodo())
+}
+
+
+export function reducer (state: stateInterface, action: any) {
+  switch (action.type) {
+    case ACTION_ADD:
+      return {
+        todoList: sortedArray([...state.todoList, action.todo])
+      }
+    case ACTION_TOGGLE_STATUS:
+      const certainTodo = state.todoList.find((todo: todo) => todo.id === action.todoId)
+      if (certainTodo === undefined) {
+        throw new Error('Cant find todo by id ' + action.todoId)
+      }
+
+      return {
+        todoList: sortedArray([
+          ...state.todoList.filter((todo) => todo.id !== action.todoId),
+          { ...certainTodo, status: certainTodo.status === STATUS_ACTIVE ? STATUS_DONE : STATUS_ACTIVE}
+        ])
+      }
+    default:
+      throw new Error('Unknown action')
+  }
+}
